14a1e65514cd05aab792226996b8969b3e3ed959
SeokJingu <98717916+SeokJingu@users.noreply.github.com>
2022-07-20 22:49:40 +0900

    loginpage add

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -15,11 +15,46 @@ class _loginpageState extends State<loginpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset('image/logo.png', width: 100, height: 100),
-          ],
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(32),
+                child: Image.asset('image/logo.png', width: 100, height: 100),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                  labelText: "이메일",
+                ),
+              ),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: "비밀번호",
+                ),
+              ),
+              Container(
+                width: double.infinity,
+                //margin: EdgeInsets.only(top: 24),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text("로그인"),
+                      ),
+                      ElevatedButton(
+                        onPressed: () {},
+                        child: Text("회원가입"),
+                      ),
+                    ]),
+              )
+            ],
+          ),
         ),
       ),
     );
