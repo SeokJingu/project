0a31ccd1ed05a5276a765894baaa31feff0ec2f7
SeokJingu <98717916+SeokJingu@users.noreply.github.com>
2022-08-06 13:50:03 +0900

    login check code add

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,204 +1,173 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
+import 'package:provider/provider.dart';
+import 'auth_service.dart';
 import 'feed.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   get children => null;
-
   get title => null;
-
   // void _onItemTapped(int index) {
-  //   setState(() {
-  //     _selectedIndex = index;
-  //   });
-  // }
-  //0■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-//   @override
-//   Widget build(BuildContext context) {
-//     return Column(
-//       children: <Widget>[
-//         _buildTop(),
-//       ],
-//     );
-//   }
-
-//   Widget _buildTop() {
-//     return Row(
-//         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//         children: <Widget>[
-//           Column(
-//             children: <Widget>[
-//               Icon(
-//                 Icons.local_taxi,
-//                 size: 40,
-//               ),
-//               Text('시나리오')
-//             ],
-//           )
-//         ]);
-//   }
-// }
-
-//0,1■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(
-          leading:
-              // IconButton(
-              //   icon: Icon(CupertinoIcons.camera, color: Colors.black),
-              //   onPressed: () {},
-              // )
-              //     Image.asset(
-              //   'image/logo.png',
-              //   height: 1,
-              // ),
-              Padding(
-            padding: const EdgeInsets.all(1),
-            child: Image.asset('image/logo.png',
-                width: 1, height: 1), //이미지 크기 조정 안됨
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: Icon(CupertinoIcons.search, color: Colors.black),
+    return Consumer<AuthService>(builder: (context, authService, child) {
+      final user = authService.currentUser();
+      return Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            leading:
+                // IconButton(
+                //   icon: Icon(CupertinoIcons.camera, color: Colors.black),
+                //   onPressed: () {},
+                // )
+                //     Image.asset(
+                //   'image/logo.png',
+                //   height: 1,
+                // ),
+                Padding(
+              padding: const EdgeInsets.all(1),
+              child: Image.asset('image/logo.png',
+                  width: 1, height: 1), //이미지 크기 조정 안됨
             ),
-            IconButton(
-              onPressed: () {},
-              icon: Icon(CupertinoIcons.bell, color: Colors.black),
-            ),
-          ],
-          //1■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-          // title: Image.asset(
-          //   'image/logo.png',
-          //   height: 32,
-          // ),
-          //2■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-          backgroundColor: Colors.white,
-        ),
-        body:
-            // Center(
-            //   child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-            //     ButtonBar(
-            //       // 버튼 바
-            //       alignment: MainAxisAlignment.start, // 중앙 정렬
-            //       buttonPadding: EdgeInsets.all(20), // 버튼의 패딩 주기
-            //       children: [
-            //         ElevatedButton(onPressed: () {}, child: Text('시나리오')),
-            //         ElevatedButton(onPressed: () {}, child: Text('편집')),
-            //       ],
-            //     )
-            //   ]),
+            actions: [
+              IconButton(
+                onPressed: () {
+                  print({user});
+                },
+                icon: Icon(CupertinoIcons.search, color: Colors.black),
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(CupertinoIcons.bell, color: Colors.black),
+              ),
+            ],
+            //1■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+            // title: Image.asset(
+            //   'image/logo.png',
+            //   height: 32,
             // ),
-            Feed(),
-        bottomNavigationBar:
-            BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '홈',
+            //2■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+            backgroundColor: Colors.white,
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: '탐색',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.star),
-            label: '인기게시물',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.chat_bubble),
-            label: '채팅',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '나의 계정',
-          ),
-        ])
-        //2■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-        // bottomNavigationBar: BottomNavigationBar(
-        //   items: const <BottomNavigationBarItem>[
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.home),
-        //       label: 'Home',
-        //     ),
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.business),
-        //       label: 'Business',
-        //     ),
-        //     BottomNavigationBarItem(
-        //       icon: Icon(Icons.school),
-        //       label: 'School',
-        //     ),
-        //   ],
-        //   currentIndex: _selectedIndex,
-        //   selectedItemColor: Colors.amber[800],
-        //   onTap: _onItemTapped,
-        // ),
-        /////////////////////////////////////////
+          body:
+              // Center(
+              //   child: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              //     ButtonBar(
+              //       // 버튼 바
+              //       alignment: MainAxisAlignment.start, // 중앙 정렬
+              //       buttonPadding: EdgeInsets.all(20), // 버튼의 패딩 주기
+              //       children: [
+              //         ElevatedButton(onPressed: () {}, child: Text('시나리오')),
+              //         ElevatedButton(onPressed: () {}, child: Text('편집')),
+              //       ],
+              //     )
+              //   ]),
+              // ),
+              Feed(),
+          bottomNavigationBar:
+              BottomNavigationBar(type: BottomNavigationBarType.fixed, items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '홈',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.chat_bubble),
+              label: '채팅',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: '나의 계정',
+            ),
+          ])
+          //2■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+          // bottomNavigationBar: BottomNavigationBar(
+          //   items: const <BottomNavigationBarItem>[
+          //     BottomNavigationBarItem(
+          //       icon: Icon(Icons.home),
+          //       label: 'Home',
+          //     ),
+          //     BottomNavigationBarItem(
+          //       icon: Icon(Icons.business),
+          //       label: 'Business',
+          //     ),
+          //     BottomNavigationBarItem(
+          //       icon: Icon(Icons.school),
+          //       label: 'School',
+          //     ),
+          //   ],
+          //   currentIndex: _selectedIndex,
+          //   selectedItemColor: Colors.amber[800],
+          //   onTap: _onItemTapped,
+          // ),
+          /////////////////////////////////////////
 
-        // bottomNavigationBar: BottomNavigationBar(
-        //   items: [
-        //     BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
-        //     BottomNavigationBarItem(icon: Icon(Icons.more), label: '더보기'),
-        //     BottomNavigationBarItem(icon: Icon(Icons.more), label: '더보기2'),
-        //   ],
-        //   onTap: (index) {
-        //     _idx = index;
-        //   },
-        //   currentIndex: _idx,
-        // ),
-        // /////////////////////////////////////////////
-        // bottomNavigationBar: Container(
-        //   color: Colors.white, //색상
-        //   child: Container(
-        //     height: 70,
-        //     padding: EdgeInsets.only(bottom: 10, top: 5),
-        //     child: const TabBar(
-        //       //tab 하단 indicator size -> .label = label의 길이
-        //       //tab 하단 indicator size -> .tab = tab의 길이
-        //       indicatorSize: TabBarIndicatorSize.label,
-        //       //tab 하단 indicator color
-        //       indicatorColor: Colors.red,
-        //       //tab 하단 indicator weight
-        //       indicatorWeight: 2,
-        //       //label color
-        //       labelColor: Colors.red,
-        //       //unselected label color
-        //       unselectedLabelColor: Colors.black38,
-        //       labelStyle: TextStyle(
-        //         fontSize: 13,
-        //       ),
-        //       tabs: [
-        //         Tab(
-        //           icon: Icon(
-        //             Icons.home_outlined,
-        //           ),
-        //           text: 'Home',
-        //         ),
-        //         Tab(
-        //           icon: Icon(Icons.music_note),
-        //           text: 'Music',
-        //         ),
-        //         Tab(
-        //           icon: Icon(
-        //             Icons.apps,
-        //           ),
-        //           text: 'Apps',
-        //         ),
-        //         Tab(
-        //           icon: Icon(
-        //             Icons.settings,
-        //           ),
-        //           text: 'Settings',
-        //         ),
-        //       ],
-        //     ),
-        //   ),
-        // ),
-        );
+          // bottomNavigationBar: BottomNavigationBar(
+          //   items: [
+          //     BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          //     BottomNavigationBarItem(icon: Icon(Icons.more), label: '더보기'),
+          //     BottomNavigationBarItem(icon: Icon(Icons.more), label: '더보기2'),
+          //   ],
+          //   onTap: (index) {
+          //     _idx = index;
+          //   },
+          //   currentIndex: _idx,
+          // ),
+          // /////////////////////////////////////////////
+          // bottomNavigationBar: Container(
+          //   color: Colors.white, //색상
+          //   child: Container(
+          //     height: 70,
+          //     padding: EdgeInsets.only(bottom: 10, top: 5),
+          //     child: const TabBar(
+          //       //tab 하단 indicator size -> .label = label의 길이
+          //       //tab 하단 indicator size -> .tab = tab의 길이
+          //       indicatorSize: TabBarIndicatorSize.label,
+          //       //tab 하단 indicator color
+          //       indicatorColor: Colors.red,
+          //       //tab 하단 indicator weight
+          //       indicatorWeight: 2,
+          //       //label color
+          //       labelColor: Colors.red,
+          //       //unselected label color
+          //       unselectedLabelColor: Colors.black38,
+          //       labelStyle: TextStyle(
+          //         fontSize: 13,
+          //       ),
+          //       tabs: [
+          //         Tab(
+          //           icon: Icon(
+          //             Icons.home_outlined,
+          //           ),
+          //           text: 'Home',
+          //         ),
+          //         Tab(
+          //           icon: Icon(Icons.music_note),
+          //           text: 'Music',
+          //         ),
+          //         Tab(
+          //           icon: Icon(
+          //             Icons.apps,
+          //           ),
+          //           text: 'Apps',
+          //         ),
+          //         Tab(
+          //           icon: Icon(
+          //             Icons.settings,
+          //           ),
+          //           text: 'Settings',
+          //         ),
+          //       ],
+          //     ),
+          //   ),
+          // ),
+          );
+    });
   }
 }
